38bae3c89fea3dce11639c2011ea41e9462d15ae
Mormonvirus <umohjamsanng19@gmail.com>
2025-04-10 22:51:47 +0100

    discover page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -4,3 +4,24 @@ window.onload = function () {
     document.getElementById('last-modified').textContent = lastModifiedDate;
 };
 
+const visitMessage = document.getElementById('visitMessage');
+const lastVisit = localStorage.getItem('lastVisit');
+const now = Date.now();
+
+if (!lastVisit) {
+    visitMessage.textContent = "Welcome! Let us know if you have any questions.";
+} else {
+    const diffInMs = now - parseInt(lastVisit);
+    const diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+
+    if (diffInDays < 1) {
+        visitMessage.textContent = "Back so soon! Awesome!";
+    } else if (diffInDays === 1) {
+        visitMessage.textContent = "You last visited 1 day ago.";
+    } else {
+        visitMessage.textContent = `You last visited ${diffInDays} days ago.`;
+    }
+}
+
+// Update the latest visit date
+localStorage.setItem('lastVisit', now);
